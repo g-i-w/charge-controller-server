2f581a48fb36889d86632888e06ed414fe06f276
G and R <gwrw@comserver.localdomain>
2022-01-31 22:38:25 -0600

    close to some basic code to get battery voltage bytes...

--- ChargeControllerServer.java
+++ ChargeControllerServer.java
@@ -4,15 +4,27 @@ import paddle.*;
 public class ChargeControllerServer extends ServerState {
 
 	private String databaseDirectory;
+	private String chargeControllerAddress;
 	
-	public ChargeControllerServer ( String databaseDirectory ) {
+	private byte[] modbusRead ( byte[] outboundBytes ) {
+		return (new OutboundTCP(
+			this,
+			chargeControllerAddress,
+			502,
+			outboundBytes
+		).receive().data();
+	}
+	
+	public ChargeControllerServer ( String databaseDirectory, String chargeControllerAddress ) {
 		this.databaseDirectory = databaseDirectory;
+		this.chargeControllerAddress = chargeControllerAddress;
 	}
 
 	public void respond ( InboundHTTP session ) {
-		count++;
+		byte[] scaleFactorBytes = modbusRead( byte[]{ 0x00, 0x00, 0x00, 0x00 }] );
+		byte[] batteryVoltageBytes = modbusRead( byte[]{ 0x00, 0x00, 0x00, 0x00 }] );		
 		session.response().setBody(
-			"<h1>HTTP works!</h1>\n<br>\n"+
+			"<h1>Battery: </h1>\n<br>\n"+
 			"path: "+session.request().path()+"\n<br>\n"+
 			"body: "+session.request().body()+"\n<br>"
 		);
@@ -20,11 +32,11 @@ public class ChargeControllerServer extends ServerState {
 	}
 	
 	public static main (String[] args) {
-		Database database = new Database( databaseDirectory );
+		//Database database = new Database( databaseDirectory );
 		ServerHTTP webUI = new ServerHTTP( this, 8080, "Charge Controller Server - Web UI" );
 		
-		while(1) {
-			new OutboundTCP(             );
-		
+		//while(1) {
+		//	new OutboundTCP(             );
+		//
 	}
 }
